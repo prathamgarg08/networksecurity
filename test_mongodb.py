bac57e1927678b2a8d5c87218850087c2254de1c
prathamgarg08 <prathamgarg89@gmail.com>
2026-01-17 18:04:57 +0530

    data pushed to mongodb atlas

---
+++ test_mongodb.py
@@ -0,0 +1,12 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+# uri = "mongodb+srv://prathamgarg:mukeshgarg@cluster0.utrdrdx.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://prathamgarg:mukeshgarg@cluster0.utrdrdx.mongodb.net/?authSource=admin"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
