1cbc88ec33310a6341e7498354f1d644ba65130e
prathamgarg08 <prathamgarg89@gmail.com>
2026-01-21 22:26:46 +0530

    Data Validation and Data Transformation completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config import DataIngestionConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -12,6 +14,20 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info('Initiate Data Ingestion')
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info('Data Ingestion completed')
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info('Initiate Data Validation')
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info('Data Validation completed')
+        print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info('Initiate Data Transformation')
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info('Data Transformation completed')
+        print(data_transformation_artifact)
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -9,6 +9,7 @@ ARTIFACT_DIR:str='Artifacts'
 FILE_NAME:str="phisingData.csv"
 TRAIN_FILE_NAME:str='train.csv'
 TEST_FILE_NAME:str='test.csv'
+SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
 
 
 DATA_INGESTION_COLLECTION_NAME:str="network_data"
@@ -17,3 +18,24 @@ DATA_INGESTION_DIRECTORY:str="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
 DATA_INGESTION_INGESTED_DIR:str="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
+
+
+DATA_VALIDATION_DIRECTORY:str="data_validation"
+DATA_VALIDATION_VALID_DIRECTORY:str="validated"
+DATA_VALIDATION_INVALID_DIRECTORY:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIRECTORY:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME:str= "preprocessing.pkl"
+
+
+DATA_TRANSFORMATION_DIR_NAME:str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str="transformed_object"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
+    "missing_values":np.nan,
+    "n_neighbors":3,
+    "weights":"uniform"
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH:str="train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH:str="test.npy"
