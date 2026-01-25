e9f0ce31f46602ecb863ae6e53e4bee52f9c2998
prathamgarg08 <prathamgarg89@gmail.com>
2026-01-25 16:31:14 +0530

    Model trainer, evaluation and mflow experiments created

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,6 +1,8 @@
 import yaml
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import os
 import sys
 import numpy as np
@@ -41,5 +43,50 @@ def save_object(file_path:str,obj:object)->None:
         with open(file_path,"wb") as file_obj:
             pickle.dump(obj,file_obj)
         logging.info("Exited save object method of main utils class")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+
+def load_object(file_path:str)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file:{file_path} does not exist")
+        with open(file_path,"rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def load_numpy_array_data(file_path:str)->np.array:
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+        
+        return report
     except Exception as e:
         raise NetworkSecurityException(e,sys)
